951ba1c61cdfa2d73f8c9ea9be7f64dc6ce1f9c2
stendor <steve.tellez@gmail.com>
2020-05-28 17:54:00 -0500

    Crud Activities API sec4

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -17,12 +15,12 @@ namespace DatingApp.API.Controllers
 
         public ValuesController(DataContext context)
         {
-            _context = context;            
+            _context = context;
         }
 
         // GET api/values
         [HttpGet]
-        public async Task< ActionResult<IEnumerable<Value>>> Get()
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
         {
             var values = await _context.Values.ToListAsync();
             return Ok(values);
@@ -30,7 +28,7 @@ namespace DatingApp.API.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public async Task <ActionResult<Value>> Get(int id)
+        public async Task<ActionResult<Value>> Get(int id)
         {
             var value = await _context.Values.FindAsync(id);
             return Ok(value);
@@ -39,19 +37,16 @@ namespace DatingApp.API.Controllers
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
-        {
-        }
+        { }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
-        {
-        }
+        { }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
-        {
-        }
+        { }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -37,6 +39,8 @@ namespace API
                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                });
            });
+           //even thou there are going to be more handlers, we just need to tell it about one since after it knows where the assebly is
+           services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();
 
         }
